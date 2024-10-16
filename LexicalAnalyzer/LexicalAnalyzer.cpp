06ff4649bb4efd5b1c8d8e3741113be5515f0072
Si11ycheese <si11ycheese6680@gmail.com>
2024-10-16 21:22:09 +0800

    finish LexicalAnalyzer

---
+++ LexicalAnalyzer/LexicalAnalyzer.cpp
@@ -0,0 +1,245 @@
+/**
+ * @file LexicalAnalyzer.cpp
+ * @brief This file contains the implementation of a lexical analyzer for PL/0.
+ * 
+ * @details
+ * The lexical analyzer reads the source code character by character, groups characters into tokens, and assigns a type to each token.
+ * It handles different types of tokens such as keywords, identifiers, numbers, and operators. It also manages errors for unrecognized characters
+ * and handles special cases like comments and whitespace.
+ * 
+ * The main functions in this file are:
+ * void scaner(): This function performs the lexical analysis by reading characters from the source code, grouping them into tokens, and assigning types.
+ * void read(): This function reads the entire source code from a file into a buffer.
+ * int main(): The main function that drives the lexical analysis process, reads the source code, calls the scanner, and outputs the results.
+ *
+ * The results of the lexical analysis are written to an output file specified by the `RESULT` macro.
+ * 
+ * @author CheeseSilly
+ * @date 2024-10-16
+ */
+#include"util.h"
+
+
+void scaner()
+{
+	
+    //initialize the token array
+	for (n = 0; n<10; n++) 
+		token[n] = NULL;
+	ch = prog[p++];
+	//skip the space
+	while (ch == ' ')
+	{
+		ch = prog[p];
+		p++;
+	}
+	//
+	if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z'))  
+	{
+		//Initialize m to 0,which is used to track the index of the token array
+		m = 0;
+		while ((ch >= '0'&&ch <= '9') || (ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z'))
+		{
+			token[m++] = ch;
+			ch = prog[p++];
+		}
+		//end the token array
+		token[m++] = '\0';
+		p--;
+		syn = 10;
+
+        //check if the token is a reserved word,all reserved words are lowercase
+		for (n = 0; n<6; n++)  
+			if (token==rwtab1[n])
+			{
+				syn = n + 1;
+				break;
+			}
+		for (n = 0; n < 4;n++)
+		{
+			if (token==rwtab2[n])
+			{
+				syn = n + 31;
+				break;
+			}
+		}
+	}
+	else if ((ch >= '0'&&ch <= '9'))  //store the number with the sum
+	{
+		sum = 0;
+		while ((ch >= '0'&&ch <= '9'))
+		{
+            //only support the integer
+			sum = sum * 10 + ch - '0';
+			ch = prog[p++];
+		}
+		p--;
+		syn = 11;
+		//max=32767
+		if (sum>32767)
+			syn = -1;
+	}
+	//check the operator and delimiter
+	else switch (ch)   
+	{
+	case '<':
+		m = 0; 
+		token[m++] = ch;
+		ch = prog[p++];
+		if (ch == '>')
+		{
+			//for <>
+			syn = 21;
+			token[m++] = ch;
+		}
+		else if (ch == '=')
+		{
+			//for <=
+			syn = 22;
+			token[m++] = ch;
+		}
+		else
+		{
+			//for <
+			syn = 23;
+			//back to the front of <
+			p--;
+		}
+		break;
+	case '>':
+		m = 0; 
+		token[m++] = ch;
+		ch = prog[p++];
+		if (ch == '=')
+		{
+			syn = 24;
+			token[m++] = ch;
+		}
+		else
+		{
+			syn = 20;
+			p--;
+		}
+		break;
+	case ':':
+		m = 0; 
+		token[m++] = ch;
+		ch = prog[p++];
+		if (ch == '=')
+		{
+			//:=
+			syn = 18;
+			token[m++] = ch;
+		}
+		else
+		{
+			//:
+			syn = 17;
+			p--;
+		}
+		break;
+	case '*':
+		syn = 13; 
+		token[0] = ch; 
+		break;
+	case '/':
+		syn = 14; 
+		token[0] = ch; 
+		break;
+	case '+':
+		syn = 15; 
+		token[0] = ch; 
+		break;
+	case '-':
+		syn = 16; 
+		token[0] = ch; 
+		break;
+	case '=':
+		syn = 25; 
+		token[0] = ch; 
+		break;
+	case ';':
+		syn = 26; 
+		token[0] = ch; 
+		break;
+	case '(':
+		syn = 27; 
+		token[0] = ch; 
+		break;
+	case ')':
+		syn = 28; 
+		token[0] = ch; 
+		break;
+	case ',':
+		syn = 29;
+		token[0] = ch;
+		break;
+	case '!':
+		syn = 30;
+		token[0] = ch;
+		break;
+	case '.':
+		//end.
+		syn = 0;
+		token[0] = ch; 
+		break;
+	case '\n':
+		//change the line,line++
+		syn = -2; 
+		break;
+	default: 
+		//undefined character,return error
+		syn = -1; 
+		break;
+	}
+}
+
+void read()
+{
+	FILE *fp;
+	fp = fopen(CODE, "r");
+	//get the size of the file
+	fseek(fp, 0, SEEK_END);
+	int file_size;
+	file_size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	prog = new char[file_size*sizeof(char)];	
+	//read the file
+	fread(prog, file_size, sizeof(char), fp);
+	fclose(fp);
+}
+
+int main()
+{
+	int p = 0;
+	int line = 1;
+	std::ofstream outfile(RESULT);
+	read();
+	p = 0;
+	do
+	{
+		scaner();
+		switch (syn)
+		{
+		case 11: 
+			outfile << "(" << syn << "," << sum << ")" << std::endl;
+			break;
+		case -1: 
+			outfile << "Error in line" << line << "!" << std::endl;
+			break;
+		case -2: 
+			line = line++; 
+			break;
+		default: 
+			outfile << "(" << syn << "," << token << ")" << std::endl;
+			break;
+		}
+	} while (syn != 0);
+
+	outfile.close();
+
+	std::cout << "complete!check it in the result.txt" << std::endl;
+
+	system("pause");
+	return 0;
+}
---
+++ LexicalAnalyzer/util.h
@@ -0,0 +1,48 @@
+/**
+ * util.h: This header file contains the function declarations and global variables used in the lexical analysis program.
+ * 
+ * @author CheeseSilly
+ * @date 2024-10-16
+ */
+# pragma once
+//test code file
+#define CODE "code.txt"
+//result file
+#define RESULT "result.txt"
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <windows.h>
+
+/**
+ * The token array is used to receive keywords, variables, operators, and delimiters.
+ * Here, the length of the variable name is limited to a maximum of 10.
+ * The prog array stores the source code string length. 
+ * @param token: The token array used to store keywords, variables, operators, and delimiters.
+ * @param prog: The prog array stores the source code string length.
+ * @param ch: The current character being processed.
+ * @param syn: The syn variable stores the number corresponding to each word or symbol.
+ * @param p: The p variable is used to track the index of the prog array.
+ * @param m: The m variable is used to track the index of the token array.
+ * @param n: The n variable is used to track the index of the rwtab1 and rwtab2 arrays.
+ * @param line: The line variable is used to track the current line number.
+ * @param sum: The sum variable is used to store the number.
+ * @param rwtab1: The rwtab1 array stores the reserved words and delimiters.
+ * @param rwtab2: The rwtab2 array stores the reserved words and delimiters.
+*/
+char token[10];
+char *prog;
+char ch;
+int syn, p, m = 0, n, line, sum = 0;
+std::string rwtab1[10] = { "begin","if","then","while","do","end" };
+std::string rwtab2[4] = { "const","var","procedure","call" };
+
+/**
+ * scaner(): This function is used to scan the source code and identify the keywords, variables, operators, and delimiters.
+ * 
+ */
+void scaner();
+
+//read the code from the file
+void read();
